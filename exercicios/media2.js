172e72c0fb457b25de83d6ea272b5426151904fb
Diego Umbelino <diego9us@gmail.com>
2024-07-26 20:50:35 -0300

    exec01

---
+++ exercicios/media2.js
@@ -0,0 +1,15 @@
+/**
+ * media com resposta aprovado
+ * @author Diego
+ */
+
+//Importar readline-sync
+
+const input = require('readline-sync')
+
+//Variaveis
+let nota1, nota2, media
+
+console.clear()
+console.log('Calculo média para aprovação: ')
+console.log(' ')
