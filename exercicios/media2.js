b7e8f2bbc28be3354c8246efb6ea661ba5aa0c0e
Diego Umbelino <diego9us@gmail.com>
2024-07-27 23:48:01 -0300

    media ex2

--- exercicios/media2.js
+++ exercicios/media2.js
@@ -12,4 +12,14 @@ let nota1, nota2, media
 
 console.clear()
 console.log('Calculo média para aprovação: ')
-nota1 = Number(input.question())
+nota1 = Number(input.question("Digite a nota 1: "))
+nota2 = Number(input.question("Digite a nota 2: "))
+//processamento
+media = (nota1 + nota2) / 2
+//Saida
+console.log(`Média: ${media.toFixed(1)}`)
+if (media >= 5.0) {
+    console.log("Aprovado! Parabéns ;)")
+} else {
+    console.log("Reprovado! Tente novamente :)")
+}
