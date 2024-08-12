f5ecdb1997f37d5004794bd225500f356ea018cc
diegowps <diego9us@gmail.com>
2024-08-12 20:19:42 -0300

    while start

---
+++ fundamentos/while.js
@@ -0,0 +1,14 @@
+/**
+ * Estudo das estruturas while e do-while
+ * @author diego
+ */
+
+const input = require(`readline-sync`)
+
+let x = 1
+let y = 1
+
+while (x < 11) {
+    console.log("teste da estrutura while")
+    x++
+}
