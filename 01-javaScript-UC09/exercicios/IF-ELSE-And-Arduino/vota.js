30ba2b54db8ac63ae6ad5cd4c2670f0bb7c5dcee
Diego Umbelino <diego9us@gmail.com>
2024-08-17 22:13:23 -0300

    reg edit

--- 01-javaScript-UC09/exercicios/IF-ELSE-And-Arduino/vota.js
+++ 01-javaScript-UC09/exercicios/IF-ELSE-And-Arduino/vota.js
@@ -10,9 +10,9 @@ console.clear
 //variaveis
 let idade
 
-idade = Number(input.question("digite a sua idade"))
+idade = Number(input.question("digite a sua idade: "))
 if (idade < 16) {
-    console.log("Proibido de voltar")
+    console.log("Sem direito a votar")
   } else if (idade >= 18 && idade < 71){
     console.log("Voto obrigatorio")
   } else {
