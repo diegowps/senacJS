af9a9d9692550e4fa6d21f4778c1d49ed40a2c05
Diego Umbelino <diego9us@gmail.com>
2024-09-20 20:31:43 -0300

    reverse car

--- 01-javaScript-UC09/POO/carro.js
+++ 01-javaScript-UC09/POO/carro.js
@@ -1,46 +1,116 @@
+
+
+
 /**
- * Classe Carro / aviao
+ * Exercício 4 / POO / carros
  * @author Diego
- * 
- * 1 - criar classe modelo (abstração)
- * 2 - criar atributos
- * 3 - criar metodos
- * 4 - criar objetos
- * 5 - adicionar um metodo construtor que gere um numero de chassi aleatorio +2 carros
- * 6 - extend classe carro criar objeto aviao
- * 7 - modificar metodo acelerar para aviao(polimorfismo)
- * 
  */
 
+
+const colors = require('colors')
+
+
+//Classe modelo (abstração)//
+
+
+
+
 class Carro {
-    constructor(ano, cor, modelo, marca) {
-        this.marca = marca;
-        this.modelo = modelo;
-        this.ano = ano;
-        this.cor = cor;
-<<<<<<< HEAD
+    
+    
+    //Atributos//
+    
+    constructor(ano, cor, modelo, chassi) {
+        this.ano = ano
+        this.cor = cor
+        this.modelo = modelo
+        this.chassi = chassi
     }
     
-    //metodos
-    //criarCarro ligarCarro desligarCarro acelerar frear
-    //criarAviao aterrizar acelerar decolar 
-
+    //Métodos//
 
 
     criarCarro() {
-        console.log("________________________");
-        console.log("Carro criado!");
-        console.log(`Marca: ${this.marca}`);
-        console.log(`Modelo: ${this.modelo}`);
+        console.log("    ______");
+        console.log(" __//  ||\\__");
+        console.log("|  _     _``-.");
+        console.log("'-(_)---(_)--'");
+        console.log(`Ano do carro: ${this.ano}`)
+        console.log(`Cor do carro: ${this.cor}`)
+        console.log(`Modelo do carro: ${this.modelo}`)
+        console.log(`Chassi do carro: ${this.chassi}`)
     }
-    dirigir() {
-        console.log("Vrum Vrum!");
+    ligarCarro() {
+        console.log(`Ligando ${this.modelo}`.blue)
     }
-
-
-
-    
+    desligarCarro() {
+        console.log(`Desligando ${this.modelo}`)
+    }    
+    acelerarCarro() {
+        console.log('Vruuuuuuuuuuuuuuuum'.bgBlack)
+        console.log('🚗')
+    }
 }
-=======
-        this.preco = preco;
->>>>>>> 8b7eee29c0a6d320ce9f361461874bc3b904d728
+
+
+//Classe modelo com herança
+
+
+
+class Aviao extends Carro {
+    
+    
+    
+    //Atributos
+    constructor(ano, cor, modelo, chassi, envergadura) {
+        super(ano, cor, modelo, chassi)
+        this.envergadura = envergadura
+    }
+    
+    
+    
+    
+    //Métodos
+    criarAviao() {
+        console.log(`Ano do avião: ${this.ano}`)
+        console.log(`Cor do avião: ${this.cor}`)
+        console.log(`Modelo do avião: ${this.modelo}`)
+        console.log(`Chassi do avião: ${this.chassi}`)
+        console.log(`Envergadura(Cumprimento de uma extremidade a outra) do avião: ${this.envergadura}`)
+    }
+    ligarAviao() {
+        console.log(`Ligando ${this.modelo}`.blue)
+    }
+    acelerarAviao() {
+        console.log(' Vruuuuuuuuuuuuuuuum '.bgRed)
+        console.log('       ✈️'.bgBlue)
+    }
+    fazerPouso() {
+        console.log('                      ✈️ '.bgGrey)
+    }
+}
+
+const carro1 = new Carro(2003, 'Branco', 'Hyunday', '4bfc5896gt9655')
+carro1.criarCarro()
+carro1.ligarCarro()
+carro1.acelerarCarro()
+carro1.desligarCarro()
+console.log('')
+const carro2 = new Carro(2023, 'Amarelo', 'Hyunday', '8TFT7855BR2323')
+carro2.criarCarro()
+carro2.ligarCarro()
+carro2.acelerarCarro()
+carro2.desligarCarro()
+console.log('')
+const aviao1 = new Aviao(2020, 'Verde', 'Boeing 777', 'BR-785', '60.90')
+aviao1.criarAviao()
+aviao1.ligarAviao()
+aviao1.acelerarAviao()
+aviao1.fazerPouso()
+console.log('')
+const aviao2 = new Aviao(2024, 'Branco', 'Novo 777', 'BR-912', '65.80')
+aviao2.criarAviao()
+aviao2.ligarAviao()
+aviao2.acelerarAviao()
+aviao2.fazerPouso()
+
